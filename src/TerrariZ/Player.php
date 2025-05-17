3cb7c71f3dd551894eaf43828b7525761faac385
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2025-05-17 10:15:36 +1000

    Update Player.php

--- src/TerrariZ/Player.php
+++ src/TerrariZ/Player.php
@@ -1,75 +1,82 @@
 <?php
-namespace TerrariZ;
 
-class Player {
-    public int $uid;               // Player Unique ID (U8)
-    public int $skinVariant;       // Skin Variant (U8)
-    public int $hair;              // Hair Style (U8)
-    public string $name;           // Player Name (String)
-    public int $hairDye;           // Hair Dye (U8)
-    public int $hideVisuals;       // Hide Visuals (U8)
-    public int $hideVisuals2;      // Hide Visuals 2 (U8)
-    public int $hideMisc;          // Hide Miscellaneous (U8)
-    public array $hairColor;       // Hair Color (3 bytes: RGB)
-    public array $skinColor;       // Skin Color (3 bytes: RGB)
-    public array $eyeColor;        // Eye Color (3 bytes: RGB)
-    public array $shirtColor;      // Shirt Color (3 bytes: RGB)
-    public array $undershirtColor; // Undershirt Color (3 bytes: RGB)
-    public array $pantsColor;      // Pants Color (3 bytes: RGB)
-    public array $shoeColor;       // Shoe Color (3 bytes: RGB)
-    public int $difficultyFlags;   // Difficulty Flags (U8)
-    public int $flags2;            // Additional Flags (U8)
-    public int $flags3;            // More Flags (U8)
+namespace TerrariZ\Internal;
 
-    public function __construct(
-        int $uid, int $skinVariant, int $hair, string $name, int $hairDye,
-        int $hideVisuals, int $hideVisuals2, int $hideMisc,
-        array $hairColor, array $skinColor, array $eyeColor,
-        array $shirtColor, array $undershirtColor, array $pantsColor, array $shoeColor,
-        int $difficultyFlags, int $flags2, int $flags3
-    ) {
-        $this->uid = $uid;
-        $this->skinVariant = $skinVariant;
-        $this->hair = $hair;
-        $this->name = $name;
-        $this->hairDye = $hairDye;
-        $this->hideVisuals = $hideVisuals;
-        $this->hideVisuals2 = $hideVisuals2;
-        $this->hideMisc = $hideMisc;
-        $this->hairColor = $hairColor;
-        $this->skinColor = $skinColor;
-        $this->eyeColor = $eyeColor;
-        $this->shirtColor = $shirtColor;
-        $this->undershirtColor = $undershirtColor;
-        $this->pantsColor = $pantsColor;
-        $this->shoeColor = $shoeColor;
-        $this->difficultyFlags = $difficultyFlags;
-        $this->flags2 = $flags2;
-        $this->flags3 = $flags3;
-    }
 
-    public function getPlayerInfo(): array {
-        return [
-            'uid' => $this->uid,
-            'skinVariant' => $this->skinVariant,
-            'hair' => $this->hair,
-            'name' => $this->name,
-            'hairDye' => $this->hairDye,
-            'hideVisuals' => $this->hideVisuals,
-            'hideVisuals2' => $this->hideVisuals2,
-            'hideMisc' => $this->hideMisc,
-            'hairColor' => $this->hairColor,
-            'skinColor' => $this->skinColor,
-            'eyeColor' => $this->eyeColor,
-            'shirtColor' => $this->shirtColor,
-            'undershirtColor' => $this->undershirtColor,
-            'pantsColor' => $this->pantsColor,
-            'shoeColor' => $this->shoeColor,
-            'difficultyFlags' => $this->difficultyFlags,
-            'flags2' => $this->flags2,
-            'flags3' => $this->flags3,
-        ];
-    }
+Class Packet {
+
+public static function sendPlayerAppearance($clientSocket, array $appearanceData) {
+    $payload = '';
+
+    $payload .= chr($appearanceData['slot']);                  // Offset 0
+    $payload .= chr($appearanceData['hairStyle']);             // Offset 1
+    $payload .= chr($appearanceData['gender']);                // Offset 2
+    $payload .= self::packColor($appearanceData['hairColor']);         // Offset 3
+    $payload .= self::packColor($appearanceData['skinColor']);         // Offset 6
+    $payload .= self::packColor($appearanceData['eyeColor']);          // Offset 9
+    $payload .= self::packColor($appearanceData['shirtColor']);        // Offset 12
+    $payload .= self::packColor($appearanceData['undershirtColor']);   // Offset 15
+    $payload .= self::packColor($appearanceData['pantsColor']);        // Offset 18
+    $payload .= self::packColor($appearanceData['shoeColor']);         // Offset 21
+    $payload .= chr($appearanceData['difficulty']);            // Offset 24
+    $payload .= self::packString($appearanceData['playerName']);       // Offset 25+
+
+    self::writePacket($clientSocket, 4, $payload);
+	
+	}
+	
+	public static function packColor(array $color): string {
+    return chr($color['r']) . chr($color['g']) . chr($color['b']);
 }
+
+public static function packString(string $str): string {
+    return chr(strlen($str)) . $str;
+}
+
+public static function readPacket($client) {
+    $lengthBytes = fread($client, 2);
+    if (!$lengthBytes) {
+        echo "No data received from client!\n";
+        return null;
+    }
+
+    $length = unpack('v', $lengthBytes)[1];
+    $packet = fread($client, $length);
+
+    if (!$packet) {
+        echo "Packet read failed!\n";
+        return null;
+    }
+
+    var_dump($packet);
+
+    return [
+        'id' => ord($packet[0]),
+        'data' => substr($packet, 1)
+    ];
+}
+
+    public static function writePacket($client, int $packetId, string $payload = '') {
+		/*
+		if ($bytesWritten = socket_write($client, $packetId) === false) {
+    echo "Socket write failed: " . socket_strerror(socket_last_error($client)) . PHP_EOL;
+}
+*/
+      stream_set_blocking($client,true);
+      $length = strlen($payload) + 3;
+	  $packet = pack('v',$length) . chr($packetId) . $payload;
+	  fwrite($client, $packet);
+	  echo "packet";
+	  var_dump($packet);
+	  echo "payload";
+	  var_dump($payload);
+	  
+	 echo "length";
+	 var_dump($length);
+	 fflush($client);
+}
+
+}
+
 
 ?>
